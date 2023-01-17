195e2459e1dfecf99aa18f0b1b28f7fa25e329ef
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2023-01-17 12:18:19 +0300

    Bug fixes

--- main.js
+++ main.js
@@ -4,27 +4,31 @@ let div = document.querySelector(".experiment");
 
 // If True (there is value in local storage)
 if (localStorage.getItem("color")) {
-    // [1] use saved color
-    div.style.backgroundColor = localStorage.getItem("color") 
 
-    // [2] remove active class from all then add it to specific <li>
-    lis.forEach((li)=>{
-        li.classList.remove("active");
-        document.querySelector(`li[data-color = ${localStorage.getItem("color")}]`).classList.add("active");
-    })
+  // [1] use saved color
+  div.style.backgroundColor = localStorage.getItem("color");
 
-}   
+  // [2] remove active class from all then add it to specific <li>
+  lis.forEach((li) => {
+    li.classList.remove("active");
+    document.querySelector(`li[data-color = ${localStorage.getItem("color")}]`).classList.add("active");
+  });
+
+}
 
 
-lis.forEach((li)=>{
-    li.addEventListener("click", (e)=>{
+lis.forEach((li) => {
 
-        lis.forEach((li)=>{
-            li.classList.remove("active");
-            e.currentTarget.classList.add("active");
-        })
+  li.addEventListener("click", (e) => {
 
-        div.style.backgroundColor = e.currentTarget.dataset.color;
-        localStorage.setItem("color", e.currentTarget.dataset.color);
-    })
-})
+    lis.forEach((li) => {
+      li.classList.remove("active");
+      e.currentTarget.classList.add("active");
+    });
+
+    div.style.backgroundColor = e.currentTarget.dataset.color;
+    localStorage.setItem("color", e.currentTarget.dataset.color);
+
+  });
+  
+});
